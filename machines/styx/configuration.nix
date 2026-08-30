56282150267d2cfcb08f2ed7f1f9952d5b86bb85
Tim Jarratt <tjarratt@gmail.com>
2026-08-30 21:20:19 +0200

    Setup postgres on styx

--- machines/styx/configuration.nix
+++ machines/styx/configuration.nix
@@ -149,6 +149,13 @@
     vim
   ];
 
+  services.postgresql = {
+    enable = true;
+    ensureUsers = [
+      { name = "postgres"; }
+    ];
+  };
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
